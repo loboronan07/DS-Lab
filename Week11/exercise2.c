8aa7d6aaa36d334e4a75ede6afe79c4991aa8946
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2022-11-26 13:49:50 +0530

    Updated L11/E2

--- Week11/exercise2.c
+++ Week11/exercise2.c
@@ -8,7 +8,7 @@
 #define MAX 20
 
 typedef struct node {
-	int data;
+	char data;
 	struct node* left;
 	struct node* right;
 } node;
@@ -18,4 +18,151 @@ typedef struct {
 	int tos;
 } stack;
 
-stack* initialize()
+node* create(char);
+void preorder(node*);
+void inorder(node*);
+void postorder(node*);
+stack* initialize();
+node* pop(stack*);
+void push(stack*, node*);
+node* atTop(stack*);
+
+int main() {
+	char data;
+	printf("Enter the data for the Tree in the order specified...\n");
+	printf("Enter data for root(Hit # to exit): ");
+	scanf(" %c", &data);
+	node* root = create(data);
+
+	printf("\nInorder Traversal of the given tree: ");
+	inorder(root);
+	printf("\n");
+
+	printf("Preorder Traversal of Tree: ");
+	preorder(root);
+	printf("\n");
+
+	printf("Postorder Traversal of the given tree: ");
+	postorder(root);
+	printf("\n");
+
+	return 0;
+}
+
+node* create(char ele){
+	if(ele == '#') 
+		return NULL;
+
+	char x;
+
+	node* temp = (node*) malloc(sizeof(node));
+	temp->data = ele;
+
+	printf("Enter data for left child of %c(Hit # to exit): ", ele);
+	scanf(" %c", &x);
+	temp->left = create(x);
+
+	printf("Enter data for right child of %c(Hit # to exit): ", ele);
+	scanf(" %c", &x);
+	temp->right = create(x);
+
+	return temp;
+}
+
+void preorder(node* root) {
+	if(!root)
+		return;
+	
+	stack* s = initialize();
+	push(s, root);
+
+	node* temp;
+	while(temp = pop(s)) {
+		printf("%c ", temp->data);
+		if(temp->right)
+			push(s, temp->right);
+		if(temp->left)
+			push(s, temp->left);
+	}
+
+	free(s);
+}
+
+void inorder(node* root) {
+	int flag = 0;
+
+	if(!root) 
+		return;
+	
+	stack* s = initialize();
+	node* curr = root;
+	
+	while(!flag) {
+		while(curr != NULL) {
+			push(s, curr);
+			curr = curr->left;
+		}
+
+		if(curr = pop(s)) {
+			printf("%c ", curr->data);
+			curr = curr->right;
+		}
+		else 
+			flag = 1;
+	}
+
+	free(s);
+}
+
+void postorder(node* root) {
+	if(!root) 
+		return;
+
+	stack* s = initialize();
+
+	do {
+		while(root) {
+			if(root->right) 
+				push(s, root->right);
+			push(s, root);
+
+			root = root->left;
+		}
+
+		root = pop(s);
+
+		if(root->right && atTop(s) == root->right) {
+			pop(s);
+			push(s, root);
+			root = root->right;
+		}
+		else {
+			printf("%d ", root->data);
+			root = NULL;
+		}
+	} while(atTop(s));
+
+	free(s);
+}
+
+stack* initialize() {
+	stack* s = (stack*) malloc(sizeof(stack));
+	s->tos = -1;
+	return s;
+}
+
+node* pop(stack* s) {
+    if(s->tos == -1)
+        return NULL;
+    return s->arr[(s->tos)--];
+}
+
+void push(stack* s, node* n) {
+    s->arr[++(s->tos)] = n;
+}
+
+node* atTop(stack* s) {
+	if(s->tos == -1)
+		return NULL;
+	return s->arr[s->tos];
+}
